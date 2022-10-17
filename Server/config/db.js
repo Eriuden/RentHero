b401f52e9296f6b6da072c9202a798ff739e8b4d
Eriuden <71089756+Eriuden@users.noreply.github.com>
2022-10-17 12:54:45 +0200

    Update db.js

--- Server/config/db.js
+++ Server/config/db.js
@@ -4,7 +4,7 @@ mongoose
     .connect(
         "mongodb+srv//" +
             process.env.DB_USER_PASS +
-            "@cluster0.iodcc.mongodb.net/RentHero"
+            "JustForMe"
     )
     .then(() => console.log("mongoDB recoit 5 sur 5"))
     .catch((err) => console.log("mongoDB n'est pas connecté", err))
